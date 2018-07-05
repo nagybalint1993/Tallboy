5b643a5c115f8f648bc8422bd7ec740400406021
nagybalint1993 <nagybalint1993@gmail.com>
2018-07-05 14:53:28 +0200

    Models added

---
+++ Tallboy/Assets/Scripts/ResistorMover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResistorMover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject gameObject = GameObject.Find("resistor1");
+        iTween.MoveBy(gameObject, iTween.Hash("y", 0.02f, "easeType", "easeInOutQuad", "loopType", "pingPong", "delay", .5));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
